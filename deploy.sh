e54a40b69c70c72aec135131e0e41f810fe98afe
LevinLFX <levin@joycastle.mobi>
2022-07-06 17:08:59 +0800

    不适用docker部署

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,5 @@
 cp -r /home/ec2-user/cert ./
-docker build -f Dockerfile -t service-monitor:latest .
-docker run -d --restart=always \
-    --name service-monitor \
-    -v /home/ec2-user/var/service-monitor/logs:/app/var/logs \
-    service-monitor:latest
+
+go build
+
+nohub ./service-monitor
